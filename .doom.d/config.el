fad190c316cdf525b8260cc926e1c3eeae40e24d
krishna1m <krishna.m.zyw@gmail.com>
2022-10-22 19:05:00 +0530

    removing unusable fonts from emacs config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -30,24 +30,12 @@
       ;; 'doom-opera
       )
 
-;; (setq doom-font (font-spec :family "Consolas" :size 20)
-;;       doom-big-font (font-spec :family "Consolas" :size 30)
-;;       doom-variable-pitch-font (font-spec :family "Consolas" :size 20)
-;;       doom-unicode-font (font-spec :family "Consolas")
-;;       doom-serif-font (font-spec :family "Consolas" :weight 'light))
 
-;; (setq doom-font (font-spec :family "Cascadia Code" :size 20)
-;;       doom-big-font (font-spec :family "Cascadia Code" :size 30)
-;;       doom-variable-pitch-font (font-spec :family "Cascadia Code" :size 20)
-;;       doom-unicode-font (font-spec :family "Cascadia Code")
-;;       doom-serif-font (font-spec :family "Cascadia Code" :weight 'light))
-
-
-(setq doom-font (font-spec :family "Ubuntu Mono" :size 15)
-      doom-big-font (font-spec :family "Ubuntu Mono" :size 20)
-      doom-variable-pitch-font (font-spec :family "Ubuntu Mono" :size 15)
-      doom-unicode-font (font-spec :family "Ubuntu Mono")
-      doom-serif-font (font-spec :family "Ubuntu Mono" :weight 'light))
+(setq doom-font (font-spec :family "Ubuntu Mono" :size 14)
+     doom-big-font (font-spec :family "Ubuntu Mono" :size 20)
+     doom-variable-pitch-font (font-spec :family "Ubuntu Mono" :size 14)
+     doom-unicode-font (font-spec :family "Ubuntu Mono")
+     doom-serif-font (font-spec :family "Ubuntu Mono" :weight 'light))
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
